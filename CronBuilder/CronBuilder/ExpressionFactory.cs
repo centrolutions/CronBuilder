07935bca2a857d6891bef6486a542aa0e6e00bf1
centrolutions <jason@centrolutions.com>
2022-01-31 21:55:29 -0600

    Added expression factory with shortcuts to commone expressions.

---
+++ CronBuilder/CronBuilder/ExpressionFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CronBuilder
+{
+    public class ExpressionFactory
+    {
+        public IExpression EveryXMinutes(int minutes)
+        {
+            var result = new Expression();
+            result.Minutes($"0/{minutes}");
+            return result;
+        }
+
+        public IExpression EveryXHours(int hours)
+        {
+            var result = new Expression();
+            result.Hours($"0/{hours}");
+            return result;
+        }
+
+        public IExpression DailyAtTime(int hour, int minutes)
+        {
+            var result = new Expression();
+            result.Minutes(minutes).Hours(hour).DayOfMonth("1/1");
+            return result;
+        }
+
+        public IExpression WeekDaysAtTime(int hour, int minutes)
+        {
+            var result = new Expression();
+            result.Minutes(minutes).Hours(hour).DayOfWeek("1-5");
+            return result;
+        }
+
+        public IExpression MonthlyOnDay(int dayOfMonth, int hour, int minutes)
+        {
+            var result = new Expression();
+            result.Minutes(minutes).Hours(hour).DayOfMonth(dayOfMonth).Months("1/1");
+            return result;
+        }
+
+        public IExpression YearlyOnDay(int month, int dayOfMonth, int hour, int minutes)
+        {
+            var result = new Expression();
+            result.Minutes(minutes).Hours(hour).DayOfMonth(dayOfMonth).Months(month);
+            return result;
+        }
+    }
+}
---
+++ CronBuilder/CronBuilderTests/ExpressionFactoryTests.cs
@@ -0,0 +1,70 @@
+﻿using CronBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CronBuilderTests
+{
+    public class ExpressionFactoryTests
+    {
+        ExpressionFactory _Sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _Sut = new ExpressionFactory();
+        }
+
+        [Test]
+        public void EveryXMinutes_ReturnsExpressionWithValueOf5_When5IsPassed()
+        {
+            var expression = _Sut.EveryXMinutes(5);
+
+            expression.Build().Should().Be("0/5 * * * *");
+        }
+
+        [Test]
+        public void EveryXHours_ReturnsExpressionWithValueOf2_When2IsPassed()
+        {
+            var expression = _Sut.EveryXHours(2);
+
+            expression.Build().Should().Be("* 0/2 * * *");
+        }
+
+        [Test]
+        public void DailyAtTime_ReturnsExpressionWithHourMinuteAndDay_WhenCalled()
+        {
+            var expression = _Sut.DailyAtTime(15, 10);
+
+            expression.Build().Should().Be("10 15 1/1 * *");
+        }
+
+        [Test]
+        public void WeekDaysAtTime_ReturnsExpressionWithHourMinuteAndWeekday_WhenCalled()
+        {
+            var expression = _Sut.WeekDaysAtTime(14, 30);
+
+            expression.Build().Should().Be("30 14 * * 1-5");
+        }
+
+        [Test]
+        public void MonthlyOnDay_ReturnsExpressionWithHourMinuteDayAndMonth_WhenCalled()
+        {
+            var expression = _Sut.MonthlyOnDay(21, 13, 45);
+
+            expression.Build().Should().Be("45 13 21 1/1 *");
+        }
+
+        [Test]
+        public void YearlyOnDay_ReturnsExpressionWithMonthDayHourAndMinute_WhenCalled()
+        {
+            var expression = _Sut.YearlyOnDay(5, 21, 20, 15);
+
+            expression.Build().Should().Be("15 20 21 5 *");
+        }
+    }
+}
